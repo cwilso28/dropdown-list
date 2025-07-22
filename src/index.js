ea46dc8bfb9f71d95b59e80b64d3240c71390714
Chris <47066922+cwilso28@users.noreply.github.com>
2025-07-21 22:24:17 -0500

    Add supporting code for list and grey background
    The user will need to be able to select from a predefined list. The list
    added is the initialization of a sample list. When the list is displayed
    on screen, the background needs to be greyed out. The code is the
    initialization of the background.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css"
+
+function createGreyBackground() {
+    let container = document.createElement("div");
+    container.id = "grey-background";
+    container.style.position = "absolute";
+    container.style.top = "0";
+    container.style.left = "0";
+    container.style.width = "100%";
+    container.style.height = "100%";
+    container.style.backgroundColor = "rgb(0 0 0 /0.5";
+
+    return container
+}
+
+function insertElement (element, position) {
+    let body = document.querySelector("body");
+    body.append(element);
+}
+
+let greyBackgroundContainer = createGreyBackground();
+insertElement(greyBackgroundContainer);
